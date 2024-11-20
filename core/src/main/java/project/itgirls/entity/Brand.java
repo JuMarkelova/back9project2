645ba9b87a6880f26836a113eb817f52d27f47af
Turaeva Viktoriya <vika.tureava.gogol@gmail.com>
2024-11-21 01:01:27 +0500

    писание изменений

---
+++ core/src/main/java/project/itgirls/entity/Brand.java
@@ -0,0 +1,66 @@
+package project.itgirls.entity;
+import java.util.Objects;
+
+public class Brand {
+    private Long id;
+    private String name;
+    private String description;
+
+    public Brand() {
+    }
+
+    public Brand(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Brand brand = (Brand) o;
+        return Objects.equals(id, brand.id) &&
+                Objects.equals(name, brand.name) &&
+                Objects.equals(description, brand.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Brand{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
+
