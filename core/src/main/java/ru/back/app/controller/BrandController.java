520059e971600ffc05e87966bcafe1a100aafd75
Aleksandra <irontree9430@gmail.com>
2024-11-22 22:57:04 +1300

    Create BrandController, add some annotations in Main

---
+++ core/src/main/java/ru/back/app/controller/BrandController.java
@@ -0,0 +1,48 @@
+package ru.back.app.controller;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.back.app.dto.BrandDto;
+
+@RestController
+@RequiredArgsConstructor
+@SecurityRequirement(name = "site-users")
+
+public class BrandController {
+
+    private final BrandService brandService; //todo
+
+    @GetMapping("/brands")
+    String getBrandsView(Model model) {
+        model.addAttribute("brands", brandService.getAllBrands());
+        return "brands";
+    }
+
+    @GetMapping("/brands/{id}")
+    BrandDto getBrandById(@PathVariable("id") Long id) {
+        return brandService.getBrandById(id);
+    }
+
+    @GetMapping("/brands/{name}")
+    BrandDto getBrandByName(@RequestParam("name") String name) {
+        return brandService.getByName(name);
+    }
+
+    @PostMapping("/brands/create")
+    BrandDto createBrand(@RequestBody @Valid BrandCreateDto brandCreateDto) {
+        return brandService.createBrand(brandCreateDto);
+    }
+
+    @PutMapping("/brands/update")
+    BrandDto updateBrand(@RequestBody @Valid BrandUpdateDto brandUpdateDto) {
+        return brandService.updateBrand(brandUpdateDto);
+    }
+
+    @DeleteMapping("/brands/delete/{id}")
+    void updateBrand(@PathVariable("id") Long id) {
+        brandService.deleteBrand(id);
+    }
+}
