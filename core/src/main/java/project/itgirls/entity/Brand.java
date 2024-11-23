20fc49c66449df04022ec664bda332d4817cb7c3
Turaeva Viktoriya <vika.tureava.gogol@gmail.com>
2024-11-23 20:10:16 +0500

    second fix

--- core/src/main/java/project/itgirls/entity/Brand.java
+++ core/src/main/java/project/itgirls/entity/Brand.java
@@ -1,10 +1,20 @@
 package project.itgirls.entity;
-import jakarta.persistence.*;
 
-import java.util.Objects;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
 
 @Entity
-@Table(name = "brands")
+@Table(name = "brand")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Builder
 
 public class Brand {
     @Id
@@ -17,60 +27,7 @@ public class Brand {
     @Column(length = 500)
     private String description;
 
-    public Brand() {
-    }
-
-    public Brand(Long id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Brand brand = (Brand) o;
-        return Objects.equals(id, brand.id) &&
-                Objects.equals(name, brand.name) &&
-                Objects.equals(description, brand.description);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, description);
-    }
-
-    @Override
-    public String toString() {
-        return "Brand{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                '}';
-    }
+    @OneToMany(mappedBy = "brand", cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude
+    private Set<Product> products;
 }
