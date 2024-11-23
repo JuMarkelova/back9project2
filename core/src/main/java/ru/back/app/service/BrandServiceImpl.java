811cc3bc246998aa0846c51b983ca0daf3adf47b
Your Name <your_email@whatever.com>
2024-11-23 14:10:54 +0500

    feature: 2024-11-23 12:11 wrote the structure for BrandService implementation

--- core/src/main/java/ru/back/app/service/BrandServiceImpl.java
+++ core/src/main/java/ru/back/app/service/BrandServiceImpl.java
@@ -2,8 +2,37 @@ package ru.back.app.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.back.app.dto.BrandDto;
+import ru.back.app.repository.BrandRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BrandServiceImpl implements BrandService{
+    private final BrandRepository brandRepository;
+
+    @Override
+    public BrandDto getBrandById(Long Id) {
+        return null;
+    }
+
+    @Override
+    public BrandDto createBrand(BrandDto brandDto) {
+        return null;
+    }
+
+    @Override
+    public List<BrandDto> getAllBrands() {
+        return List.of();
+    }
+
+    @Override
+    public BrandDto updateBrand(BrandDto brandDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteBrand(Long id) {
+    }
 }
