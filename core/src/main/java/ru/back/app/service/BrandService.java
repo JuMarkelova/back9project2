8e9e02c74cad993c55fa2f4728e8e3c46a6140d3
Your Name <your_email@whatever.com>
2024-11-23 14:08:13 +0500

    feature: 2024-11-23 12:07 created interdace BrandService with not implemented crud functions

---
+++ core/src/main/java/ru/back/app/service/BrandService.java
@@ -0,0 +1,12 @@
+package ru.back.app.service;
+
+import ru.back.app.dto.BrandDto;
+import java.util.List;
+
+public interface BrandService {
+    BrandDto getBrandById(Long Id);
+    BrandDto createBrand(BrandDto brandDto);
+    List<BrandDto> getAllBrands();
+    BrandDto updateBrand(BrandDto brandDto);
+    void deleteBrand(Long id);
+}
---
+++ core/src/main/java/ru/back/app/service/BrandServiceImpl.java
@@ -0,0 +1,9 @@
+package ru.back.app.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BrandServiceImpl implements BrandService{
+}
