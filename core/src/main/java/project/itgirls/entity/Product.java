c1c8306b2feb798e5dcc98cb154818cae7bfc55c
Turaeva Viktoriya <vika.tureava.gogol@gmail.com>
2024-11-23 20:18:44 +0500

    second fix

--- core/src/main/java/project/itgirls/entity/Product.java
+++ core/src/main/java/project/itgirls/entity/Product.java
@@ -1,10 +1,16 @@
 package project.itgirls.entity;
-import java.util.Objects;
-
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Table(name = "products")
+@Table(name = "product") // Исправлено имя таблицы
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Builder
 
 public class Product {
     @Id
@@ -17,75 +23,8 @@ public class Product {
     @Column(nullable = false)
     private Double price;
 
-    @Column(name = "brand_id", nullable = false)
-    private Long brandId;
-
-    // Конструкторы, геттеры, сеттеры, equals(), hashCode() и toString()
-    public Product() {
-    }
-
-    public Product(Long id, String name, Double price, Long brandId) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.brandId = brandId;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Long getBrandId() {
-        return brandId;
-    }
-
-    public void setBrandId(Long brandId) {
-        this.brandId = brandId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Product product = (Product) o;
-        return Objects.equals(id, product.id) &&
-                Objects.equals(name, product.name) &&
-                Objects.equals(price, product.price) &&
-                Objects.equals(brandId, product.brandId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, price, brandId);
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", price=" + price +
-                ", brandId=" + brandId +
-                '}';
-    }
+    // Связь с Brand через @ManyToOne
+    @ManyToOne
+    @JoinColumn(name = "brand_id", nullable = false) // Связываем с колонкой brand_id
+    private Brand brand;
 }
