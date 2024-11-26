009c8d8f8d8f7e11f960b0ffefa16f31e8451b80
Aleksandra <irontree9430@gmail.com>
2024-11-26 15:35:49 +1300

    delete annotations, change getAllBrands method

--- core/src/main/java/ru/back/app/Application.java
+++ core/src/main/java/ru/back/app/Application.java
@@ -1,16 +1,9 @@
 package ru.back.app;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Brands API", version = "2.0", description = "Brands Information"))
-@SecurityScheme(name = "site-users", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
