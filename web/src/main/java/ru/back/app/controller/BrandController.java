a179115a0dee059687448e7d21c88efdd31a2ce1
Your Name <your_email@whatever.com>
2024-12-02 11:45:49 +0500

    feature 9:45 02/12/2024 created requests in web BrandController that are delegated by coreClient

--- web/src/main/java/ru/back/app/controller/BrandController.java
+++ web/src/main/java/ru/back/app/controller/BrandController.java
@@ -1,10 +1,10 @@
 package ru.back.app.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.back.app.client.CoreClient;
+import ru.back.app.dto.BrandCreateDto;
 import ru.back.app.dto.BrandDto;
 
 import java.util.List;
@@ -18,5 +18,30 @@ public class BrandController {
     @GetMapping
     public List<BrandDto> getAllBrands(){
         return coreClient.getAllBrands();
+    }
+
+    @GetMapping("/id/{id}")
+    public BrandDto getBrandById(@PathVariable("id") Long id){
+        return coreClient.getBrandById(id);
+    }
+
+    @GetMapping("/name")
+    public BrandDto getBrandByName(@RequestParam(value = "name", required = false) String name){
+        return coreClient.getBrandByName(name);
+    }
+
+    @PostMapping("/create")
+    public BrandDto createBrand(@RequestBody @Valid BrandCreateDto brandCreateDto){
+        return coreClient.createBrand(brandCreateDto);
+    }
+
+    @PutMapping("/update")
+    public BrandDto updateBrand(@RequestBody @Valid BrandDto brandUpdateDto){
+        return coreClient.updateBrand(brandUpdateDto);
+    }
+    
+    @DeleteMapping("/delete/{id}")
+    public void deleteBrand(@PathVariable("id") Long id){
+        coreClient.deleteBrand(id);
     }
 }
