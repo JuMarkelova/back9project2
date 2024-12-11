6035eaf6cbaf10095de783d8da78aec418d1b4be
Your Name <your_email@whatever.com>
2024-12-12 01:05:53 +0500

    feature: created a configuration class for redis

---
+++ web/src/main/java/ru/back/app/config/RedisConfig.java
@@ -0,0 +1,18 @@
+package ru.back.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+    g
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
+
