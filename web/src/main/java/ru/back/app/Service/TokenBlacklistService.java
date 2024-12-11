651e798f062f433646ff0ac021970514a07b33c0
Your Name <your_email@whatever.com>
2024-12-12 01:31:31 +0500

    feature: created add, delete and boolean functions ffor tokens in blacklist management

--- web/src/main/java/ru/back/app/Service/TokenBlacklistService.java
+++ web/src/main/java/ru/back/app/Service/TokenBlacklistService.java
@@ -1,6 +1,5 @@
 package ru.back.app.Service;
 
-import org.antlr.v4.runtime.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -13,6 +12,16 @@ public class TokenBlacklistService {
     public TokenBlacklistService(RedisTemplate<String, String> redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
-    
 
+    public void addTokenToBlacklist(String token) {
+        redisTemplate.opsForValue().set(token, "blacklisted");
+    }
+    
+    public void removeTokenFromBlacklist(String token) {
+        redisTemplate.delete(token);
+    }
+
+    public boolean isTokenBlacklisted(String token) {
+        return redisTemplate.hasKey(token);
+    }
 }
