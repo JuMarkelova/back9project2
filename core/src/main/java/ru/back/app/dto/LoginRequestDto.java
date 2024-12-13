28fef3ff7eeeb1420b05dc2c0c6bf1f79709b8df
Iuliia <markelova.uv@gmail.com>
2024-12-13 18:40:49 +0200

    add user validation

--- core/src/main/java/ru/back/app/dto/LoginRequestDto.java
+++ core/src/main/java/ru/back/app/dto/LoginRequestDto.java
@@ -1,5 +1,8 @@
 package ru.back.app.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @AllArgsConstructor
@@ -8,6 +11,11 @@ import lombok.*;
 @Builder
 @Getter
 public class LoginRequestDto {
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email must not be empty")
     private String email;
+
+    @NotBlank(message = "Password must not be empty")
+    @Size(min = 4)
     private String password;
 }
--- core/src/main/java/ru/back/app/dto/UserDto.java
+++ core/src/main/java/ru/back/app/dto/UserDto.java
@@ -1,6 +1,10 @@
 package ru.back.app.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,10 +15,23 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 public class UserDto {
+    @NotBlank(message = "Name must not be empty")
+    @Size(max = 50)
     private String name;
+
+    @NotBlank(message = "Surname must not be empty")
+    @Size(max = 50)
     private String surname;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email must not be empty")
     private String email;
+
+    @NotBlank(message = "Password must not be empty")
+    @Size(min = 4)
     private String password;
+
+    @NotNull
     @JsonProperty("roleId")
     private Long roleId;
 }
